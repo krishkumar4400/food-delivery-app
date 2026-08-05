700d7ff162100679459797cae10bdc1488d3451e
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-08-05 21:47:54 +0530

    Add auth controller
    standardize api error and api response using oop principle

--- backend/src/modules/auth/controller/auth.controller.js
+++ backend/src/modules/auth/controller/auth.controller.js
@@ -4,8 +4,8 @@ import { asyncHandler } from "../../../utils/asyncHandler.js";
 
 const loginController = asyncHandler(async(req,res) => {
     const {username, email, password, role} = req.body;
-})
+});
 
 const registerController = asyncHandler(async(req,res) => {
     const {username, email, password, role} = req.body;
-})
+});
